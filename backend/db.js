de65dd49a80fe85747dd99ee5d8f42aca3597e20
Yogesh.project <yk76430@gmail.com>
2026-02-02 22:28:53 +0530

    fix port for render

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 
-
 const connectDB = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/dsa_sheet");
-    console.log("MongoDB Connected");
-};
+  try {
+    const MONGO_URI = process.env.MONGO_URI;
 
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+
+    await mongoose.connect(MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
+
 const app = express();
 connectDB();
 
@@ -11,7 +12,6 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/progress", require("./routes/progress"));
- 
 
-
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
