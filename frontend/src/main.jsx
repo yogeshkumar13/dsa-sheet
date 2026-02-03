5ba57b5ba8bdcbc51baf8e39b5b04d2993fbb329
Yogesh.project <yk76430@gmail.com>
2026-02-03 11:18:09 +0530

    ____

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import { UserProvider } from "./context/UserContext";
